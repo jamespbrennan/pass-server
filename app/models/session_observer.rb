a2038060997ac8c63e6e55c822e22da1a6f2b345
James Brennan <james@jamesbrennan.ca>
2013-03-18 18:13:11 -0700

    Added basic node server code. Node and rails are now talking via redis

--- app/models/session_observer.rb
+++ app/models/session_observer.rb
@@ -2,7 +2,7 @@ class SessionObserver < ActiveRecord::Observer
   
   def after_update(session)
     if(session.is_authenticated_changed?)
-      data = {id: session.id, is_authenticated: session.is_authenticated}
+      data = {session_id: session.id, is_authenticated: session.is_authenticated}
 
       $redis.publish 'session_updated', data.to_json
     end
