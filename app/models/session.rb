6ea10823fd3240a9994883540148e4db69a2e8a0
James Brennan <james@jamesbrennan.ca>
2013-03-20 11:07:37 -0700

    Removed device_id change check for sessions.

--- app/models/session.rb
+++ app/models/session.rb
@@ -19,34 +19,25 @@ class Session < ActiveRecord::Base
   belongs_to :device
 
   validates :service_id, :presence => true
-  validate :preserve_device_id
 
   before_create :generate_token
 
   private
 
-  # Generate Token
+  # == Generate Token
   #
   # Create the nonce for the session
   #
+
   def generate_token
 		self.token = SecureRandom.hex
   end
 
-  # Preserve Device ID
-  #
-  # Do not let device ids that have been set, be updated
-  #
-  def preserve_device_id
-    if(self.device_id_changed? && self.device_id_was != nil)
-      errors.add(:device_id, 'You cannot update a device id that has already been set.')
-    end
-  end
-
-  # Report Updated
+  # == Report Updated
   #
   # Send message to notify the browser that the session has been updated
   #
+
   def report_updated
     data = {id: self.id, is_authenticated: self.is_authenticated}
 
