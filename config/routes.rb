6bab2c3d616b24144512668ff86505cfa076b1b9
James Brennan <james@jamesbrennan.ca>
2013-03-21 11:17:41 -0700

    Cleaned up routes. Added catch all for API to display a JSON 404 message

--- config/routes.rb
+++ config/routes.rb
@@ -2,26 +2,7 @@ require 'api_constraints'
 
 PassServer::Application.routes.draw do
 
-  scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-    constraints :subdomain => 'api' do
-
-      # Give them a blank page as the default route so it doesn't look like the API is down
-      root to: proc { [200, {}, ['']] }
-
-      post 'sessions/create' => 'sessions#create'
-      get 'sessions/get' => 'sessions#get'
-      post 'sessions/authenticate' => 'sessions#authenticate'
-
-      post 'devices/create' => 'devices#create'
-      post 'devices/register' => 'devices#register'
-
-      post 'users/create' => 'users#create'
-    end
-  end
-
-  constraints :subdomain => '' do
-    root to: 'devices#index'
-
+  constraints(:subdomain => /^(|www)$/) do
     get 'signup', to: 'users#new', as: 'signup'
     get 'login', to: 'sessions#new', as: 'login'
     get 'logout', to: 'sessions#destroy', as: 'logout'
@@ -29,53 +10,29 @@ PassServer::Application.routes.draw do
     resources :users
     resources :sessions
     resources :devices
+
+    root to: 'devices#index'
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  constraints :subdomain => 'api' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+    
+      # Give them a blank page as the default route so it doesn't look like the API is down
+      root to: proc { [200, {}, ['']] }
 
-  # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+      post 'sessions' => 'sessions#create'
+      get 'sessions' => 'sessions#get'
+      post 'sessions/authenticate' => 'sessions#authenticate'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+      post 'devices' => 'devices#create'
+      post 'devices/register' => 'devices#register'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+      post 'users' => 'users#create'
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+      get '*a' => 'errors#routing'
+      post '*a' => 'errors#routing'
+      delete '*a' => 'errors#routing'
+      patch '*a' => 'errors#routing'
+    end
+  end
 end
