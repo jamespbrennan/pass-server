d189612db50444bd16d497b5f143eaa3c9d7c6e4
James Brennan <james@jamesbrennan.ca>
2013-04-07 16:59:57 -0700

    Trying asset fix for capistrano rails 4

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,3 +49,14 @@ namespace :deploy do
   end
   before "deploy", "deploy:check_revision"
 end
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => assets_role, :except => { :no_release => true } do
+      run <<-CMD.compact
+        cd -- #{latest_release.shellescape} &&
+        #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile
+      CMD
+    end
+  end
+end
