edb99916a92cc00a8de10ca82accd78c5b380991
James Brennan <james@jamesbrennan.ca>
2013-04-29 15:32:10 -0700

    More node debugging

--- realtime/messenger.js
+++ realtime/messenger.js
@@ -14,6 +14,9 @@ redis.on('message', function(channel, message) {
 
   console.log(message);
 
+  io.sockets.in("session_" + message.session_id).emit("channel", "message");
+  io.sockets.in("session_" + message.session_id).emit("message", "message");
+
   if( typeof message.session_id != "undefined") {
     // Send the message only to the clients in the session_id room
     // channel is an event, likey `session_updated`, `message` is the JSON for the event
@@ -25,6 +28,6 @@ io.sockets.on('connection', function (socket) {
   socket.on('subscribe', function(session_id) {
     // Join the client to a room based on their session_id
     socket.join("session_" + session_id);
-    console.log("Clients in:  " + io.sockets.in("session_" + session_id));
+    console.log("Clients in:  " + io.sockets.in("session_" + session_id).length);
   });
 });
