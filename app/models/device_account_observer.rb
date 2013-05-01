a91619188cfb0fd407cb73c6ebb48d4c46adba62
James Brennan <james@jamesbrennan.ca>
2013-05-01 13:46:01 -0700

    Added observer to tell service when a device account is registered

---
+++ app/models/device_account_observer.rb
@@ -0,0 +1,27 @@
+class DeviceAccountObserver < ActiveRecord::Observer
+  
+  def after_update(device_account)
+    data = { id: device_account.user.id, email: device_account.user.email }.to_json
+
+    #TODO Should this be done with delayed jobs? Will it get to the service in time? How slow is this for 2+ callbacks?
+    # Tell the service of the user creation
+    service = device_account.service
+    callbacks = service.callbacks.all(conditions: { callback_type_id: CallbackType.find_by(name: 'user_creation') })
+
+    callbacks.each do |callback|
+      begin
+        uri = URI.parse callback
+
+        request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+        request.body = data
+        response = Net::HTTP.new(uri.host, uri.port).start do |http|
+          http.request(request)
+        end
+        
+      rescue
+        #TODO Notify service that the callback is bad, maybe create a delayed job? Probably too late for that
+      end
+    end if callbacks
+  end
+  
+end
