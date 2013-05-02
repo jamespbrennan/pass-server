63f419c9c91388df4a851d761fe9bb5a0a6221df
James Brennan <james@jamesbrennan.ca>
2013-05-02 14:36:02 -0700

    Moved after commit back, found record.previous_changes that can be used after commit

--- app/models/session.rb
+++ app/models/session.rb
@@ -23,9 +23,7 @@ class Session < ActiveRecord::Base
   validates :service_id, :presence => true
 
   before_create :generate_token
-  before_update :set_authenticated_at
-
-  after_commit :notify_parties, :if => Proc.new { |session| session.is_authenticated_changed? && session.is_authenticated }
+  before_save :set_authenticated_at
 
   private
 
@@ -40,42 +38,6 @@ class Session < ActiveRecord::Base
 
   def set_authenticated_at
     self.authenticated_at = Time.zone.now if self.is_authenticated_changed?
-  end
-
-  def notify_parties(session)
-    data = { session_id: session.id, is_authenticated: session.is_authenticated }.to_json
-
-    #TODO Should this be done with delayed jobs? Will it get to the service in time? How slow is this for 2+ callbacks?
-    # Tell the service of the successful authentication
-    service = session.service
-    callbacks = service.callbacks.all(conditions: { callback_type_id: CallbackType.find_by(name: 'authentication') })
-
-    callbacks.each do |callback|
-      begin
-        uri = URI.parse callback.address
-
-        request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
-        request.body = data
-
-        http = Net::HTTP.new(uri.host, uri.port)
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-        response = http.start do |h|
-          h.request(request)
-        end
-        
-      rescue => e
-        #TODO Notify service that the callback is bad, maybe create a delayed job? Probably too late for that
-        session.logger.error e.to_s
-        session.logger.error response.to_s
-      end
-    end if callbacks
-
-    # Tell node of the change
-    $redis.publish 'session_updated', data
-
-    true
   end
 
 end
--- app/models/session_observer.rb
+++ app/models/session_observer.rb
@@ -1,3 +1,41 @@
 class SessionObserver < ActiveRecord::Observer
   
+  def after_commit(session)
+    if(session.previous_changes.include?(:is_authenticated) && session.is_authenticated)
+      data = { session_id: session.id, is_authenticated: session.is_authenticated }.to_json
+
+      #TODO Should this be done with delayed jobs? Will it get to the service in time? How slow is this for 2+ callbacks?
+      # Tell the service of the successful authentication
+      service = session.service
+      callbacks = service.callbacks.all(conditions: { callback_type_id: CallbackType.find_by(name: 'authentication') })
+
+      callbacks.each do |callback|
+        begin
+          uri = URI.parse callback.address
+
+          request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
+          request.body = data
+
+          http = Net::HTTP.new(uri.host, uri.port)
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+          response = http.start do |h|
+            h.request(request)
+          end
+          
+        rescue => e
+          #TODO Notify service that the callback is bad, maybe create a delayed job? Probably too late for that
+          session.logger.error e.to_s
+          session.logger.error response.to_s
+        end
+      end if callbacks
+
+      # Tell node of the change
+      $redis.publish 'session_updated', data
+    end
+
+    true
+  end
+  
 end
