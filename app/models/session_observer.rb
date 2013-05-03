800419a659dfbd91e3f7474548edd41200221d57
James Brennan <james@jamesbrennan.ca>
2013-05-02 21:17:16 -0700

    Switched callback address usage

--- app/models/session_observer.rb
+++ app/models/session_observer.rb
@@ -16,7 +16,7 @@ class SessionObserver < ActiveRecord::Observer
           request = Net::HTTP::Post.new(uri.path, initheader = {'Content-Type' =>'application/json'})
           request.body = data
 
-          http = Net::HTTP.new(callback.address)
+          http = Net::HTTP.new(uri.host, uri.port)
 
           if uri.class == URI::HTTPS
             http.use_ssl = true
